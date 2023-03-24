6fc78c3bb1d32fe73b0b04d6c9392d9d4bdcf6df
Julia B <julia.bernst@gmail.com>
2023-03-23 23:16:29 -0400

    Fixed db and table functions

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,14 +1,29 @@
--- I will need to generate a ton of fake employees
-
 INSERT INTO department (name)
-VALUES ("Anthropology"),
-       ("Biological Sciences"),
-       ("Computer and Mathematical Sciences")
-       ("English"),
-       ("Health and Society"),
-       ("Philosophy"),
-       ("Political Science"),
-
+VALUES ("Sales"),
+       ("Risk Management"),
+       ("Computers and Math"),
+       ("Finance"),
+       ("Crib"),
+       ("Legal"),
+       ("The Top");
 
 INSERT INTO role (title, salary, department_id)
-VALUES ("")
+VALUES  ("Sales Lead", 100000, 7),
+        ("Salesperson", 50000, 1),
+        ("Lead Engineer", 20000, 2),
+        ("Software Engineer", 10101, 3),
+        ("Account Manager", 180000, 4),
+        ("Chief Executive Officer", 9000000, 6),
+        ("Lawyer", 50000, 5);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES  ("Eric", "Stateman", 2, NULL),
+        ("Jennifer", "Coolidge", 7, NULL),
+        ("Nancy", "Dreifus", 1, 1),
+        ("Peter", "Porkins", 4, 2),
+        ("Bart", "Harley Jarvis", 5, NULL),
+        ("Julie", "Woodin", 2, 2),
+        ("Alice", "Nelson", 3, 5),
+        ("Alice", "Anderson", 4, 1),
+        ("Edward", "From Twilight", 5, 2),
+        ("Mister", "Rich", 6, NULL);
--- index.js
+++ index.js
@@ -1,164 +1,8 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql2");
 require("dotenv").config();
-require("console.table");
+const ctable = require("console.table");
 
-// Ask if we can use sequelize
-
-// connect to sql with promise in order to use async / await
-// consider using a pool for better practice
-const db = mysql
-  .createConnection(
-    // Use dotenv to protect privacy
-    {
-      host: "localhost",
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
-      database: "employees_db",
-    },
-    console.log(`Connected to the employees_db database.`)
-  )
-  .promise();
-
-// Query functions should be in a separate file
-
-// Return all instances in a given table
-const getList = async (table) => {
-  const list = await db.query(`SELECT * FROM ${table}`);
-  return list;
-};
-
-// Prints a given table
-function viewTable(table) {
-  db.query(`SELECT * FROM ${table}`, function (err, results) {
-    console.table(results);
-  });
-}
-
-function addDepartment() {
-  // Get input
-  const questions = [
-    {
-      type: "input",
-      name: "name",
-      message: "Enter the name of the department:",
-    },
-  ];
-
-  inquirer.prompt(questions).then((answers) => {
-    db.query(`INSERT INTO departments (name) VALUES (${answers.name})`).then(
-      viewTable("departments")
-    );
-  });
-}
-
-function addRole() {
-  // Get input
-  const questions = [
-    {
-      type: "input",
-      name: "title",
-      message: "Enter the title for the role:",
-    },
-    {
-      type: "input",
-      name: "salary",
-      message: "Enter the salary for the role:",
-    },
-    {
-      type: "search-list",
-      name: "department",
-      message: "Enter the department for the role:",
-      choices: getList("departments"),
-    },
-  ];
-
-  inquirer.prompt(questions).then((answers) => {
-    db.query(
-      `INSERT INTO roles (title, salary, department) VALUES (${answers.title}, ${answers.salary}, ${answers.department})`
-    ).then(viewTable("roles"));
-  });
-}
-
-function addEmployee() {
-  // Get input
-  const questions = [
-    {
-      type: "input",
-      name: "first_name",
-      message: "Enter the first name of the employee:",
-    },
-    {
-      type: "input",
-      name: "last_name",
-      message: "Enter the last name of the employee:",
-    },
-    {
-      type: "search-list",
-      name: "role",
-      message: "Enter the role of the employee:",
-      choices: getList("roles"),
-    },
-    {
-      type: "confirm",
-      name: "hasManager",
-      message: "Does this employee have a manager?",
-    },
-    {
-      type: "search-list",
-      message: "Enter the name of the manager:",
-      name: "manager",
-      choices: getList("employees"),
-      when: (answers) => answers.hasManager,
-    },
-  ];
-
-  inquirer
-    .prompt(questions)
-    .then((answers) => {
-      if (answers.hasManager) {
-        db.query(
-          `INSERT INTO employees (first_name, last_name, role_id, manager) VALUES (${answers.first_name}, ${answers.last_name}, ${answers.role.id}, ${answers.manager.id})`
-        );
-      } else {
-        db.query(
-          `INSERT INTO employees (first_name, last_name, role_id, manager) VALUES (${answers.first_name}, ${answers.last_name}, ${answers.role.id}, NULL)`
-        );
-      }
-    })
-    .then(viewTable("employees"));
-}
-
-function updateEmployee() {
-  inquirer
-    .prompt(
-      {
-        type: "search-list",
-        message: "Enter the employee's name:",
-        name: "employee",
-        choices: getList("employees"),
-      },
-      {
-        type: "search-list",
-        message: "What would you like their new role to be?",
-        name: "role",
-        choices: getList("roles"),
-      }
-    )
-    .then((answers) => {
-      db.query(
-        `UPDATE employees SET role_id = ${answers.role.id} WHERE id = ${answers.employee.id}`
-      );
-    });
-
-  viewTable("employees");
-}
-
-// TODO: Create a function to initialize app
-// call the questions asynchronously
-// after each response, present the options again ("What would you like to do next?")
-// As long as they didn't choose Quit
-// Myabe use loop instead of calling the prompt over and over
 const options = [
   {
     type: "list",
@@ -177,42 +21,257 @@ const options = [
   },
 ];
 
-async function prompt() {
-  const getNext = await inquirer.prompt(options);
-  // All of these functions can be exported from another file to keep this shit clean
+const db = mysql.createConnection(
+  // Use dotenv to protect privacy
+  {
+    host: "localhost",
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: "employee_db",
+  },
+  console.log(`Connected to the employee_db database`)
+);
 
-  switch (getNext) {
-    case "View all departments":
-      viewTable("departments");
-      break;
-    case "View all roles":
-      viewTable("roles");
-      break;
-    case "View all employees":
-      viewTable("employees");
-      break;
-    case "Add a department":
-      addDepartment();
-      break;
-    case "Add a role":
-      addRole();
-      break;
-    case "Add an employee":
-      addEmployee();
-      break;
-    case "Update an employee role":
-      updateEmployee();
-      break;
-    case "Quit":
-      break;
-  }
+// Query functions should be in a separate file
 
-  if (getNext === "Quit") {
-    console.log("Goodbye!");
-  } else {
-    // Otherwise, prompt again
-    prompt();
-  }
+// Return all rows of a given table
+// Should only get the name or title
+const getDepartmentNames = () => {
+  db.query("SELECT name FROM department", function (err, result) {
+    return result;
+  });
+};
+
+const getRoleTitles = () => {
+  db.query("SELECT title FROM role", function (err, result) {
+    return result;
+  });
+};
+
+const getEmployeeNames = () => {
+  db.query(
+    "SELECT first_name, last_name FROM employee",
+    function (err, results) {
+      for (var i=0; i<results.length; i++){
+        results[i].concat();
+      }
+    }
+  );
+};
+
+// Print tables
+function viewDepartments() {
+  db.query(`SELECT * FROM department`, function (err, results) {
+    console.table(results);
+  });
+  prompt();
+}
+
+function viewRoles() {
+  // Join role with the department names
+  db.query(
+    `SELECT role.id, role.title, role.salary AS id, title, salary, department.name AS department FROM role JOIN department ON role.department_id = department.id`,
+    function (err, results) {
+      console.table(results);
+    }
+  );
+  prompt();
+}
+
+function viewEmployees() {
+  db.query(
+    `SELECT employee.id, employee.first_name, employee.last_name, employee.manager_id AS id, first_name, last_name, manager_id, role.title AS title FROM employee JOIN role ON employee.role_id = role.id`,
+    function (err, results) {
+      console.table(results);
+    }
+  );
+  prompt();
+}
+
+function addDepartment() {
+  // Get input
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "Enter the name of the department:",
+      },
+    ])
+    .then((answers) => {
+      db.query(
+        `INSERT INTO department (name) VALUES (${answers.name})`,
+        function (err, results) {
+          //if (err) throw err;
+          console.log(`Added ${answers.name} to the database`);
+        }
+      );
+    });
+  prompt();
+}
+
+function addRole() {
+  // Get input
+  const questions = [
+    {
+      type: "input",
+      name: "title",
+      message: "Enter the title for the role:",
+    },
+    {
+      type: "input",
+      name: "salary",
+      message: "Enter the salary for the role:",
+    },
+    {
+      type: "list",
+      name: "department",
+      message: "Enter the department for the role:",
+      choices: [getDepartmentNames()],
+    },
+  ];
+
+  inquirer.prompt(questions).then((answers) => {
+    const department_id = db.query(
+      `SELECT id FROM department WHERE name=?`,
+      answers.department
+    );
+    db.query(
+      `INSERT INTO role (title, salary, department_id) VALUES (${answers.title}, ${answers.salary}, ${department_id})`
+    ).then(console.log(`Added ${answers.title} to the database`));
+  });
+  prompt();
+}
+
+async function addEmployee() {
+  // Get
+  const managers = db.query(
+    "SELECT first_name, last_name FROM employee WHERE NOT manager_id = null"
+  );
+
+  // Get role titles
+  const titles = [db.query("SELECT title FROM role")];
+
+  const questions = [
+    {
+      type: "input",
+      name: "first_name",
+      message: "What is the first name of the employee? ",
+    },
+    {
+      type: "input",
+      name: "last_name",
+      message: "What is the last name of the employee? ",
+    },
+    {
+      type: "list",
+      name: "role",
+      message: "What is the role of the employee? ",
+      choices: titles,
+    },
+    {
+      type: "confirm",
+      name: "hasManager",
+      message: "Does this employee have a manager? ",
+    },
+    {
+      type: "list",
+      message: "What is the name of the manager? ",
+      name: managers,
+      choices: managers,
+      when: (answers) => answers.hasManager,
+    },
+  ];
+
+  inquirer.prompt(questions).then((answers) => {
+    const role_id = db.query(
+      `SELECT id FROM role WHERE title='${answers.role}'`
+    );
+    if (answers.hasManager) {
+      const manager_id = db.query(
+        `SELECT id FROM employee WHERE first_name=${answers.employee}`
+      );
+      db.query(
+        `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (${answers.first_name}, ${answers.last_name}, ${role_id}, ${manager_id})`
+      );
+    } else {
+      db.query(
+        `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (${answers.first_name}, ${answers.last_name}, ${role_id}, NULL)`
+      );
+    }
+
+    console.log(
+      `Added ${answers.first_name} ${answers.last_name} to the database`
+    );
+  });
+  prompt();
+}
+
+function updateEmployee() {
+  let employees = db.query(
+    "SELECT first_name, last_name AS firstName, lastName FROM employee"
+  );
+
+  inquirer
+    .prompt(
+      {
+        type: "search-list",
+        message: "Enter the name of the employee you'd like to update",
+        name: "employee",
+        choices: getEmployeeNames(),
+      },
+      {
+        type: "search-list",
+        message: "What would you like their new role to be?",
+        name: "role",
+        choices: getRoleTitles(),
+      }
+    )
+    .then((answers) => {
+      const role_id = db.query(`SELECT id FROM role WHERE title=?`, [
+        answers.role,
+      ]);
+
+      db.query(
+        `UPDATE employee SET role_id = ${role_id} WHERE first_name = ${answers.employee}` //Parse answers.employee to get the first and last name
+      );
+
+      console.log(
+        `Updated the file of ${answers.first_name} ${answers.last_name}`
+      );
+    });
+  prompt();
+}
+
+function prompt() {
+  inquirer.prompt(options).then((answer) => {
+    switch (answer.next) {
+      case "View all departments":
+        viewDepartments();
+        break;
+      case "View all roles":
+        viewRoles();
+        break;
+      case "View all employees":
+        viewEmployees();
+        break;
+      case "Add a department":
+        addDepartment();
+        break;
+      case "Add a role":
+        addRole();
+        break;
+      case "Add an employee":
+        addEmployee();
+        break;
+      case "Update an employee role":
+        updateEmployee();
+        break;
+      case "Quit":
+        console.log("Goodbye!");
+        return;
+    }
+  });
 }
 
 // Function call to begin the prompt
